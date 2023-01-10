92db81e956baf66e65914af94cd223a90897695f
Kris van Rens <50581423+krisvanrens@users.noreply.github.com>
2023-01-10 15:05:35 +0100

    CHG: Update exercise code.

--- code/module_01/exercise2.cpp
+++ code/module_01/exercise2.cpp
@@ -2,6 +2,15 @@
 
 int main() {
   // Exercise: verify correct setting of values..
+
+  // Example verification result:
+  //
+  //       MSB                                    |                   |         |      LSB
+  //  RAX: 0000'0001'0010'0011'0100'0101'0110'0111'1000'1001'1010'1011'1100'1101'1110'1111
+  //  EAX:                                         1000'1001'1010'1011'1100'1101'1110'1111
+  //   AX:                                                             1100'1101'1110'1111
+  //   AL:                                                                       1110'1111
+  //   AH:                                                             1100'1101
 }
 
-// Compiler Explorer: https://www.godbolt.org/z/fvn4cWqdj
+// Compiler Explorer: https://www.godbolt.org/z/4nGc9j8br
